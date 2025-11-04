32ae998ac6ff7d6dfb65a871f8edf7cfd3f964ee
Muad-syntax <mohammad.suaebummuad9@smk.belajar.id>
2025-11-04 09:00:08 +0700

    konfersi Celcius ke Fahrenheit

---
+++ konfersi_suhu.py
@@ -0,0 +1,4 @@
+celcius = float(input("Masukan suhu dalam Celcius: "))
+fahrenheit = (celcius * 9/5) + 32
+print(f"{celcius} derajat Celcius sama dengan {fahrenheit:.2f} derajat Fahrenheit")
+print()

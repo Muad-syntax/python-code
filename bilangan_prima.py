c4c1e9615e1e604fa20cb13747959d165efde601
art-syntax <mohammad.suaebummuad9@smk.belajar.id>
2025-11-06 07:39:26 +0700

    Update bilangan_prima.py

--- bilangan_prima.py
+++ bilangan_prima.py
@@ -7,7 +7,7 @@ elif num > 1:
     for i in range(2,num):
         if num % i == 0:
             flag = True
-        break
+            break
     if flag:
         print(num, "bukan bilangan prima")
     else:
